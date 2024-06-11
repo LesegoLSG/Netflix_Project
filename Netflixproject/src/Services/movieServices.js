659a52f01415d183cc98fdf2eff6204bf347d4f1
LesegoLSG <lesegomhlongo78@gmail.com>
2024-06-11 16:39:50 -0500

    Tmdb endpoints for getting movies

--- Netflixproject/src/Services/movieServices.js
+++ Netflixproject/src/Services/movieServices.js
@@ -1,6 +1,7 @@
-const key = import.meta.env.VITE_TMDB_KEY;
-const baseUrl = "https://api.themoviedb.org/3";
+const key = import.meta.env.VITE_TMDB_KEY;// Get the API key from environment variables
+const baseUrl = "https://api.themoviedb.org/3";// Base URL for TMDb API
 
+// Object containing various endpoints for TMDb API
 const endpoints = {
     popular:`${baseUrl}/movie/popular?api_key=${key}`,
     topRated:`${baseUrl}/movie/top_rated?api_key=${key}`,
@@ -8,9 +9,9 @@ const endpoints = {
     comedy:`${baseUrl}/movie/popular?api_key=${key}&language=US&query=comedy&page=1&include_adult=false`,
     upcoming:`${baseUrl}/movie/upcoming?api_key=${key}`,
 }
-
+// Function to create image URLs using the TMDb image base URL and size
 export function createImageUrl(filename,size){
     return `https://image.tmdb.org/t/p/${size}/${filename}`;
 }
-
+// Export the endpoints object and the createImageUrl function
 export default endpoints;
