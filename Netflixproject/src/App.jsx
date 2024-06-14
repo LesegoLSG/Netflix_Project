3edc0ce356b61633c0bead8f7ca5dcfc27153b4f
LesegoLSG <lesegomhlongo78@gmail.com>
2024-06-14 10:54:20 -0500

    Added a route for trailer

--- Netflixproject/src/App.jsx
+++ Netflixproject/src/App.jsx
@@ -7,16 +7,20 @@ import SignUp from "./Pages/SignUp";
 import Profile from "./Pages/Profile";
 import { AuthContextProvider } from "./Context/AuthContext";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import Trailer from "./Pages/Trailer";
 
 function App() {
   return (
     <>
+      {/* Wrap the entire application in the AuthContextProvider to provide authentication context */}
       <AuthContextProvider>
         <Navbar />
+        {/* Define the routes using the Routes and Route components from react-router-dom */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          {/* Protected route for the Profile page, accessible only when user is authenticated */}
           <Route
             path="/profile"
             element={
@@ -25,6 +29,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route path="/trailer/:id" element={<Trailer />} />
         </Routes>
       </AuthContextProvider>
     </>
